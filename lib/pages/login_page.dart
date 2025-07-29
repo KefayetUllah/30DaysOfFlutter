d6fc757b4a4df9840e8440fedca8e24cb21b0ec6
Kefayet Ullah <kefayet107@gmail.com>
2025-07-30 01:57:34 +0600

    Initial Commit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,15 +4,14 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
+      child: Column(
+        children: [
+          Image.asset("assets/images/Login_Image.png", fit: BoxFit.cover),
+          Text(
+            "Welcome",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
           ),
-        ),
+        ],
       ),
     );
   }
