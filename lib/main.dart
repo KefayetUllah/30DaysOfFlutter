3bf4269f8f49bb225eb70b15bce0d65e19e4624d
Kefayet Ullah <kefayet107@gmail.com>
2025-07-29 14:04:20 +0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/home_page.dart';
+import 'package:flutter_application/pages/home_page.dart';
+import 'package:flutter_application/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/homepage": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
   }
 }
